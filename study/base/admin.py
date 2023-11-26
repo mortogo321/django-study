8d032791332858a31a416cf69017b73087dd12b4
Mor <mortogo321@gmail.com>
2023-11-27 00:30:11 +0700

    add models

--- study/base/admin.py
+++ study/base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from base.models import Message, Room, Topic
+
 # Register your models here.
+admin.site.register(Message)
+admin.site.register(Room)
+admin.site.register(Topic)
